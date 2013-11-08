e25208ff5b7cdadf774dd1e16f671e0b0ad18eec
hsgw <urkwtky@gmail.com>
2013-11-08 23:42:18 +0900

    add manuals etc.

--- doc/html/search/all_70.js
+++ doc/html/search/all_70.js
@@ -1,6 +1,8 @@
 var searchData=
 [
   ['pinmode',['pinMode',['../class_g7___digital64.html#af34304bf601331a977f2b5d620e1514c',1,'G7_Digital64::pinMode(uint8_t _ic, uint8_t _port, uint8_t _pin, uint8_t _value)'],['../class_g7___digital64.html#aeb473edab1c03db9b2945302ea7a0a94',1,'G7_Digital64::pinMode(uint8_t _ic, uint8_t _pin, uint8_t _value)']]],
-  ['portread',['portRead',['../class_g7___digital64.html#a000cd97a83b8fac24fa0d62dd52491bc',1,'G7_Digital64']]],
-  ['portwrite',['portWrite',['../class_g7___digital64.html#a79086f428d9a38689d71038414050ae7',1,'G7_Digital64']]]
+  ['portread',['portRead',['../class_g7___digital64.html#a55778fc0a8e683e1345a3321f32558f1',1,'G7_Digital64']]],
+  ['portreadall',['portReadAll',['../class_g7___digital64.html#af9ef498a46ec36e6f4d53a62b412fa0c',1,'G7_Digital64']]],
+  ['portwrite',['portWrite',['../class_g7___digital64.html#a79086f428d9a38689d71038414050ae7',1,'G7_Digital64']]],
+  ['portwriteall',['portWriteAll',['../class_g7___digital64.html#acaa91ea86e43b5d0f5e43d3e53fcb06a',1,'G7_Digital64']]]
 ];
--- doc/html/search/all_73.js
+++ doc/html/search/all_73.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['setconfig',['setConfig',['../class_g7___digital64.html#abc8c4261f939cbe7e9a2783c9b53c613',1,'G7_Digital64']]],
+  ['setconfigall',['setConfigAll',['../class_g7___digital64.html#ad5fb7b1be25cc311eae654f910334ff4',1,'G7_Digital64']]],
   ['setpolarity',['setPolarity',['../class_g7___digital64.html#a60f02254f0fd3d84961c99de055203c2',1,'G7_Digital64']]]
 ];
